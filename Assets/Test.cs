532e948987c24920534e210b4e1ea47f9d2f35b5
jorgeskamijo <reincarnation404@yahoo.co.jp>
2017-03-30 01:26:15 +0900

    ScriptTestプロジェクト　課題
    Lesson4の課題を作成

---
+++ Assets/Test.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        // 配列を初期化する
+        int[] array = { 30, 50, 80, 60, 100 };
+
+        // 配列の要素数のぶんだけ処理を繰り返す
+        for (int i = 0; i < array.Length; i++)
+        {
+         
+                // 配列の要素を表示する
+                Debug.Log(array[i]);
+            
+        }
+        // 配列の要素数のぶんだけ処理を繰り返す
+        for (int t = 4; t >= 0; t--)
+        {
+
+            // 配列の要素を表示する
+            Debug.Log(array[t]);
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/kadai2.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss
+{
+    private int mp = 53;          // 魔力
+    // 魔法用の関数
+    public void Magic(int syouhi)
+    {
+        if (mp >= syouhi)
+        {
+            // 残りmpを減らす
+            this.mp -= syouhi;
+            // MPの値が消費以上の場合、文字列を表示する
+            Debug.Log("魔法攻撃をした。残りMPは" + mp);
+        }
+        else
+        {
+            Debug.Log("MPが足りないため魔法が使えない"); ;
+        }
+
+    }
+
+}
+
+public class kadai2 : MonoBehaviour
+{
+
+    void Start()
+    {
+        // Bossクラスの変数を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
+        // 11回処理を繰り返す
+        for (int i = 0; i <= 10; i++)
+        {
+
+            // 魔法用の関数を呼び出す
+            lastboss.Magic(5);
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
